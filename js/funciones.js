1567780125d0b203e7c801ca3454cc3ed2099a53
Ricardo499 <perazon19@hotmail.com>
2021-11-17 22:35:53 -0700

    parte 19

--- js/funciones.js
+++ js/funciones.js
@@ -1,16 +1,14 @@
 //Archivo de funciones de JavaScript, Cuidado con moverle mucho!!!
 
 function confirmar() {
-    if (confirm("Va a eliminar un registro, �est� usted seguro?")) {
         return true;
     }
 
-}
+
 
 function confirmarModificar(){
-    if (confirm("Va a Modificar un registro, �est� usted seguro?")) {
         return true;
     }
 
-}
+
 
