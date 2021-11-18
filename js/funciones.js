96ac915bc17a621e2fdd0ec22f2fe99351aff0d1
Ricardo499 <perazon19@hotmail.com>
2021-11-17 22:33:50 -0700

    parte 18

--- js/funciones.js
+++ js/funciones.js
@@ -4,13 +4,13 @@ function confirmar() {
     if (confirm("Va a eliminar un registro, �est� usted seguro?")) {
         return true;
     }
-    return false;
+
 }
 
 function confirmarModificar(){
     if (confirm("Va a Modificar un registro, �est� usted seguro?")) {
         return true;
     }
-    return false;
+
 }
 
